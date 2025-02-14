1b3315d19631661215a93e222d4f0b0b0b47280e
Jesse Soares <a.l.i.t.t.l.e@hotmail.com>
2025-02-14 08:59:23 -0300

    add category views and serializer

--- inventory_manager/urls.py
+++ inventory_manager/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from product.views import ProductViewSet, CategoryViewSet
+
+router = routers.DefaultRouter()
+router.register('product', ProductViewSet, basename="Produtos")
+router.register('category', CategoryViewSet, basename="Categoria")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('product.urls'))
+    path('api/', include(router.urls))
 ]
